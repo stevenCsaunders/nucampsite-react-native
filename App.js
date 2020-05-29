7f60b1587206526cfa1d657547550ef10a852a3d
Steven Saunders <stevensaunders@stevens-mbp.lan>
2020-05-28 21:22:15 -0600

    Component part 2

--- App.js
+++ App.js
@@ -1,22 +1,20 @@
-import React from 'react';
-import Main from './components/MainComponent';
-import { StyleSheet} from 'react-native';
+import React from 'react'
+import Main from './components/MainComponent'
+import { StyleSheet } from 'react-native'
 
 export default function App() {
-  return (
-   <Main />
-  )
+	return <Main />
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#123343',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    fontSize: 80,
-    color: '#fff',
-  },
-});
+	container: {
+		flex: 1,
+		backgroundColor: '#123343',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	text: {
+		fontSize: 80,
+		color: '#fff',
+	},
+})
--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,20 +1,41 @@
-import React, { Component } from 'react';
-import Directory from './DirectoryComponent';
-import { CAMPSITES } from '../shared/campsites';
+import React, { Component } from 'react'
+import Directory from './DirectoryComponent'
+import CampsiteInfo from './CampsiteInfoComponent'
+import { View } from 'react-native'
+import { CAMPSITES } from '../shared/campsites'
 
 class Main extends Component {
-    constructor(props) {
-        super(props);
+	constructor(props) {
+		super(props)
 
-        this.state = {
-            campsites: CAMPSITES
-        };
-    }
-    render() {
-        return(
-            <Directory campsites={this.state.campsites} />
-        );
-    }
+		this.state = {
+			campsites: CAMPSITES,
+			selectedCampsite: null,
+		}
+	}
+
+	onCampsiteSelect(campsiteId) {
+		this.setState({ selectedCampsite: campsiteId })
+	}
+
+	render() {
+		return (
+			<View style={{ flex: 1 }}>
+				<Directory
+					campsites={this.state.campsites}
+					onPress={(campsiteId) => this.onCampsiteSelect(campsiteId)}
+				/>
+				<CampsiteInfo
+					campsite={
+						this.state.campsites.filter(
+							(campsite) =>
+								campsite.id === this.state.selectedCampsite
+						)[0]
+					}
+				/>
+			</View>
+		)
+	}
 }
 
-export default Main;
+export default Main
