bdfc901e2f3e5c05fa1b0c8201e595eccc6beaea
Steven Saunders <stevensaunders@stevens-mbp.lan>
2020-05-30 00:14:47 -0600

    Navigation Part 1

--- App.js
+++ App.js
@@ -1,20 +1,8 @@
 import React from 'react'
 import Main from './components/MainComponent'
-import { StyleSheet } from 'react-native'
+
+console.disableYellowBox = true;
 
 export default function App() {
 	return <Main />
 }
-
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: '#123343',
-		alignItems: 'center',
-		justifyContent: 'center',
-	},
-	text: {
-		fontSize: 80,
-		color: '#fff',
-	},
-})
