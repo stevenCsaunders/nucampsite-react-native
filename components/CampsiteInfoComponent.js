c2b2e7a851e1cd6df7cda1ef032838e490b37baa
Steven Saunders <stevensaunders@stevens-mbp.lan>
2020-06-04 20:42:09 -0600

    Favorites

--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -1,40 +1,44 @@
 import React, { Component } from 'react'
 import { Text, View, ScrollView, FlatList } from 'react-native'
 import { Card, Icon } from 'react-native-elements'
-import { CAMPSITES } from '../shared/campsites'
-import { COMMENTS } from '../shared/comments'
-import { connect } from 'react-redux';
-import { baseUrl } from '../shared/baseUrl';
+import { connect } from 'react-redux'
+import { baseUrl } from '../shared/baseUrl'
+import { postFavorite } from '../redux/ActionCreators'
 
-const mapStateToProps = state => {
-    return {
-        campsites: state.campsites,
-        comments: state.comments
-    };
-};
+const mapStateToProps = (state) => {
+	return {
+		campsites: state.campsites,
+		comments: state.comments,
+		favorites: state.favorites,
+	}
+}
+
+const mapDispatchToProps = {
+	postFavorite: (campsiteId) => postFavorite(campsiteId),
+}
 
 function RenderCampsite(props) {
-    const { campsite } = props
+	const { campsite } = props
 	if (campsite) {
 		return (
-<Card
-		featuredTitle={campsite.name}
-		image={{uri: baseUrl + campsite.image}}
-	>
-		<Text style={{ margin: 10 }}>{campsite.description}</Text>
-		<Icon
-			name={props.favorite ? 'heart' : 'heart-o'}
-			type="font-awesome"
-			color="#f50"
-			raised
-			reverse
-			onPress={() =>
-				props.favorite
-					? console.log('Already set as a favorite')
-					: props.markFavorite()
-			}
-		/>
-	</Card>
+			<Card
+				featuredTitle={campsite.name}
+				image={{ uri: baseUrl + campsite.image }}
+			>
+				<Text style={{ margin: 10 }}>{campsite.description}</Text>
+				<Icon
+					name={props.favorite ? 'heart' : 'heart-o'}
+					type="font-awesome"
+					color="#f50"
+					raised
+					reverse
+					onPress={() =>
+						props.favorite
+							? console.log('Already set as a favorite')
+							: props.markFavorite()
+					}
+				/>
+			</Card>
 		)
 	}
 	return <View />
@@ -64,39 +68,41 @@ function RenderComments({ comments }) {
 	)
 }
 
-
 class CampsiteInfo extends Component {
 	constructor(props) {
 		super(props)
-        this.state = {
-            favorite: false
-        };
-
+		this.state = {
+			favorite: false,
+		}
 	}
 
 	static navigationOptions = {
 		title: 'Campsite Information',
-    }
-    
+	}
 
-    markFavorite() {
-        this.setState({favorite: true});
-    }
+	markFavorite(campsiteId) {
+		this.props.postFavorite(campsiteId)
+	}
 
 	render() {
-        const campsiteId = this.props.navigation.getParam('campsiteId');
-        const campsite = this.props.campsites.campsites.filter(campsite => campsite.id === campsiteId)[0];
-        const comments = this.props.comments.comments.filter(comment => comment.campsiteId === campsiteId);
-        return (
-            <ScrollView>
-                <RenderCampsite campsite={campsite}
-                    favorite={this.state.favorite}
-                    markFavorite={() => this.markFavorite()}
-                />
-                <RenderComments comments={comments} />
-            </ScrollView>
+		const campsiteId = this.props.navigation.getParam('campsiteId')
+		const campsite = this.props.campsites.campsites.filter(
+			(campsite) => campsite.id === campsiteId
+		)[0]
+		const comments = this.props.comments.comments.filter(
+			(comment) => comment.campsiteId === campsiteId
+		)
+		return (
+			<ScrollView>
+				<RenderCampsite
+					campsite={campsite}
+					favorite={this.props.favorites.includes(campsiteId)}
+					markFavorite={() => this.markFavorite(campsiteId)}
+				/>
+				<RenderComments comments={comments} />
+			</ScrollView>
 		)
 	}
 }
 
-export default connect(mapStateToProps)(CampsiteInfo);
+export default connect(mapStateToProps, mapDispatchToProps)(CampsiteInfo)
--- redux/ConfigureStore.js
+++ redux/ConfigureStore.js
@@ -5,15 +5,17 @@ import { campsites } from './campsites'
 import { comments } from './comments'
 import { promotions } from './promotions'
 import { partners } from './partners'
+import { favorites } from './favorites';
 
 export const ConfigureStore = () => {
 	const store = createStore(
-		combineReducers({
-			campsites,
-			comments,
-			partners,
-			promotions,
-		}),
+        combineReducers({
+            campsites,
+            comments,
+            partners,
+            promotions,
+            favorites
+        }),
 		applyMiddleware(thunk, logger)
 	)
 
