b788109eaa06d9d71cbdf10e2a5f8e62e345b57d
Steven Saunders <stevensaunders@stevens-mbp.lan>
2020-06-04 20:31:16 -0600

    Activity Indicator

--- components/DirectoryComponent.js
+++ components/DirectoryComponent.js
@@ -1,50 +1,63 @@
-import React, { Component } from 'react';
-import { FlatList } from 'react-native';
-import { Tile } from 'react-native-elements';
-import { connect } from 'react-redux';
-import { baseUrl } from '../shared/baseUrl';
+import React, { Component } from 'react'
+import { Tile } from 'react-native-elements'
+import { connect } from 'react-redux'
+import { baseUrl } from '../shared/baseUrl'
+import { View, FlatList } from 'react-native'
+import Loading from './LoadingComponent'
 
-const mapStateToProps = state => {
-    return {
-        campsites: state.campsites
-    };
-};import { CAMPSITES } from '../shared/campsites';
+const mapStateToProps = (state) => {
+	return {
+		campsites: state.campsites,
+	}
+}
+import { CAMPSITES } from '../shared/campsites'
 
 class Directory extends Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			campsites: CAMPSITES,
+		}
+	}
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            campsites: CAMPSITES
-        };
-    }
+	static navigationOptions = {
+		title: 'Directory',
+	}
 
-    static navigationOptions = {
-        title: 'Directory'
-    };
+	render() {
+		const { navigate } = this.props.navigation
+		const renderDirectoryItem = ({ item }) => {
+			return (
+				<Tile
+					title={item.name}
+					caption={item.description}
+					featured
+					onPress={() =>
+						navigate('CampsiteInfo', { campsiteId: item.id })
+					}
+					imageSrc={{ uri: baseUrl + item.image }}
+				/>
+			)
+		}
 
-    render() {
-        const { navigate } = this.props.navigation;
-        const renderDirectoryItem = ({item}) => {
-            return (
-                <Tile
-                    title={item.name}
-                    caption={item.description}
-                    featured
-                    onPress={() => navigate('CampsiteInfo', { campsiteId: item.id })}
-                    imageSrc={{uri: baseUrl + item.image}}
-                />
-            );
-        };
-
-        return (
-            <FlatList
-                data={this.props.campsites.campsites}
-                renderItem={renderDirectoryItem}
-                keyExtractor={item => item.id.toString()}
-            />
-        );
-    }
+		if (this.props.campsites.isLoading) {
+			return <Loading />
+		}
+		if (this.props.campsites.errMess) {
+			return (
+				<View>
+					<Text>{this.props.campsites.errMess}</Text>
+				</View>
+			)
+		}
+		return (
+			<FlatList
+				data={this.props.campsites.campsites}
+				renderItem={renderDirectoryItem}
+				keyExtractor={(item) => item.id.toString()}
+			/>
+		)
+	}
 }
 
-export default connect(mapStateToProps)(Directory);
+export default connect(mapStateToProps)(Directory)
